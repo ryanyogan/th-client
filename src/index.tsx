ee0819e7416684c1c952db801e3e87c3ce56c1fb
Ryan Yogan <ryanyogan@gmail.com>
2020-12-08 09:21:19 -0600

    fixed ts version, added a mock component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { Listings } from "./sections";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <Listings />
   </React.StrictMode>,
   document.getElementById("root")
 );
