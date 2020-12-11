ef7b44369fa645c0c27dc24fe989cf95a8605371
Ryan Yogan <ryanyogan@gmail.com>
2020-12-11 10:39:15 -0600

    added a skeleton, errors, and some ant design

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Listings } from "./sections";
+import "./styles/index.css";
 
 const client = new ApolloClient({
   uri: "/api",
--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -5,6 +5,9 @@ import {
   DeleteListing as DeleteListingData,
   DeleteListingVariables,
 } from "./__generated__/DeleteListing";
+import { Alert, Avatar, Button, List, Spin } from "antd";
+import "./styles/Listings.css";
+import { ListingsSkeleton } from "./components";
 
 const LISTINGS = gql`
   query Listings {
@@ -49,44 +52,63 @@ export const Listings = ({ title }: Props) => {
   };
 
   if (loading) {
-    return <h2>Loading...</h2>;
+    return (
+      <div className="listings">
+        <ListingsSkeleton title={title} />
+      </div>
+    );
   }
 
   if (error) {
-    return <h2>Uh oh! Something went wrong -- please try again.</h2>;
+    return (
+      <div className="listings">
+        <ListingsSkeleton title={title} error />
+      </div>
+    );
   }
 
   const listings = data ? data.listings : null;
 
-  const deleteListingLoadingMessage = deleteLoading ? (
-    <h4>Deletion in progress...</h4>
-  ) : null;
-
-  const deleteListingErrorMessage = deleteError ? (
-    <h4>Uh Oh! Something went wrong :(</h4>
+  const deleteListingErrorAlert = deleteError ? (
+    <Alert
+      type="error"
+      message="Uh oh! Something went wrong. Please try again."
+      className="listings__alert"
+    />
   ) : null;
 
   const listingsList = listings ? (
-    <ul>
-      {listings.map((listing) => {
-        return (
-          <li key={listing.id}>
-            {listing.title}
-            <button onClick={() => handleDeleteListing(listing.id)}>
+    <List
+      itemLayout="horizontal"
+      dataSource={listings}
+      renderItem={(listing) => (
+        <List.Item
+          actions={[
+            <Button
+              type="primary"
+              onClick={() => handleDeleteListing(listing.id)}
+            >
               Delete
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+            </Button>,
+          ]}
+        >
+          <List.Item.Meta
+            title={listing.title}
+            description={listing.address}
+            avatar={<Avatar src={listing.image} shape="square" size={48} />}
+          />
+        </List.Item>
+      )}
+    />
   ) : null;
 
   return (
-    <div>
-      <h2>{title}</h2>
-      {listingsList}
-      {deleteListingLoadingMessage}
-      {deleteListingErrorMessage}
+    <div className="listings">
+      {deleteListingErrorAlert}
+      <Spin spinning={deleteLoading}>
+        <h2>{title}</h2>
+        {listingsList}
+      </Spin>
     </div>
   );
 };
