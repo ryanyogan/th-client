7b65c16bf4d693bd9c1142ab816da1b9c7c6a161
Ryan Yogan <ryanyogan@gmail.com>
2020-12-15 16:38:14 -0600

    added routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Listings } from "./sections";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/index.css";
 
 const client = new ApolloClient({
@@ -10,10 +11,25 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/host" render={() => <div>Host</div>} />
+        <Route exact path="/listing/:id" render={() => <div>Listing</div>} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" render={() => <div>User</div>} />
+        <Route render={() => <div>Not Found</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Listings title="TinyHouse Listings" />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
